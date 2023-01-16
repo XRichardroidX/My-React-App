33a22b8fb799eac6b31905b7ded4645b586ff66b
XRichardroidX <oguzieibehrichard@gmail.com>
2023-01-16 07:30:07 +0100

    Made changes

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
-      <div>
-          <h4>Version 1.0.0</h4>
-          <Link to="/">Go Back</Link>
+    <div>
+      <h4>Version 2.0.1</h4>
+      <Link to="/">Go Back</Link>
     </div>
   )
 }
